2b96e118f8081adf5a492798ba09bb45ff00f2ed
ackhack <christoph.fuerbacher@gmx.de>
2021-12-15 14:49:40 +0100

    logging imported, more animes

--- import.js
+++ import.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const FILEURL = "./malCache.json";
+const FILEURL = process.env.USERPROFILE + "/Downloads/malCache.json";
 
 importCache(FILEURL);
 
@@ -21,11 +21,14 @@ function importCache(file) {
     for (let id in imported) {
         if (storage[id]) {
             for (let site in imported[id]) {
-                if (site in storage[id]) {
+                if (!(site in storage[id])) {
+                    console.log("Importing ID:" + id + " || " + site + ": " + imported[id][site]);
                     storage[id][site] = imported[id][site];
                 }
             }
         } else {
+            for (let site in imported[id])
+                console.log("Importing ID:" + id + " || " + site + ": " + imported[id][site]);
             storage[id] = imported[id];
         }
     }
