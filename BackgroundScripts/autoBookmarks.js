4d8889c24be837919c7869ec6ebb4fa8ce431028
ackhack <christoph.fuerbacher@gmx.de>
2022-08-13 14:02:25 +0200

    upgraded autoBookmarks fetch

--- BackgroundScripts/autoBookmarks.js
+++ BackgroundScripts/autoBookmarks.js
@@ -8,21 +8,14 @@ function bookmarkLoop() {
 
         getSitesVariable(sites => {
             getPreferredSiteNameVariable(preferredSiteName => {
-                let site = sites[preferredSiteName];
-                fetch(site.updatePageURL).then(res => {
-                    return res.text();
-                }).then(body => {
-                    processBookmarkLoopBody(site, body);
-                }).catch(err => {
-                    console.log(err);
-                });
+                processBookmarkLoop(sites[preferredSiteName]);
             });
         });
 
     });
 }
 
-async function processBookmarkLoopBody(site, body) {
+async function processBookmarkLoop(site) {
     getAnimeCacheVariable(async animeCache => {
         getHistoryVariable(async historyObj => {
             getBookmarkAutoNotificationVariable(async bookmarkAutoNotification => {
@@ -53,96 +46,7 @@ async function processBookmarkLoopBody(site, body) {
                     }
                 }
 
-                let regex = undefined;
-                let match = undefined;
-                let addedAnimes = [];
-
-                switch (site.siteName) {
-                    case "kickassanime":
-                        regex = /\s*appData\s*=\s*(\{.*\})\s*\|\|\s*\{\}/;
-                        match = body.match(regex);
-                        if (match != null) {
-                            let json = match[1];
-                            let list = JSON.parse(json).animeList;
-                            for (let anime of list.sub.concat(list.dub)) {
-                                let ret = await tryAddAnime(site.prefixURL + anime.slug.substring(1));
-                                if (ret) {
-                                    addedAnimes.push(ret);
-                                }
-                            }
-                        }
-                        break;
-                    case "9anime":
-                        //new site
-                        regex = /<div\s+id="list-items"\s+class="ani items">((.|\n)*)<\/div>(.|\n)+<nav>/;
-                        match = regex.exec(body);
-                        if (match) {
-                            let regexA = /<a\s+href="([^"]+)">((.|\n)*?)<\/a>/g;
-                            let regexDS = /data-status-sub="EP (\d+)(\/\d+)?"/gi;
-                            let matchA = undefined;
-                            let checkURLs = [];
-                            while (matchA = regexA.exec(match[1])) {
-                                if (matchA[1].startsWith("/watch/")) {
-                                    if (!checkURLs.includes(matchA[1])) {
-                                        checkURLs.push(matchA[1]);
-                                        let matchDS = regexDS.exec(matchA[2]);
-                                        if (matchDS) {
-                                            let ret = await tryAddAnime(site.prefixURL + matchA[1] + "/ep-" + matchDS[1]);
-                                            if (ret) {
-                                                addedAnimes.push(ret);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            break;
-                        }
-                        //old site
-                        regex = /<ul\s+class="anime-list">((.|\n)*)<\/ul>\s*\n?\s*<div\s+class="pagenav">/;
-                        match = regex.exec(body);
-                        if (match) {
-                            let regexA = /<a\s+href="([^"]+)"\s+class="poster"(.|\n)*?>((.|\n)*?)<\/a>/g;
-                            let regexDS = /Ep (Full|\d+)/g;
-                            let matchA = undefined;
-                            let checkURLs = [];
-                            while (matchA = regexA.exec(match[1])) {
-                                if (matchA[1].startsWith("/watch/")) {
-                                    if (!checkURLs.includes(matchA[1])) {
-                                        checkURLs.push(matchA[1]);
-                                        let matchDS = regexDS.exec(matchA[3]);
-                                        //without this line, the script won't match every other instance
-                                        matchA[3].match(regexDS)
-                                        if (matchDS) {
-                                            let ret = await tryAddAnime(site.prefixURL + matchA[1] + "/ep-" + matchDS[1]);
-                                            if (ret) {
-                                                addedAnimes.push(ret);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        break;
-                    case "gogoanimehub":
-                        regex = /<div\s+class="last_episodes loaddub">((.|\n)*)<\/div>\s+<\/div>\s+<\/div>\s+<div\s+class="clr">/
-                        match = regex.exec(body);
-
-                        if (match) {
-                            let regexA = /<a\s+href="([^"]+)"\s*\n?\s*title=.*>/g;
-                            let matchA = undefined;
-                            let checkURLs = [];
-                            while (matchA = regexA.exec(match[1])) {
-                                if (!checkURLs.includes(matchA[1])) {
-                                    checkURLs.push(matchA[1]);
-                                    let ret = await tryAddAnime(site.prefixURL + matchA[1]);
-                                    if (ret) {
-                                        addedAnimes.push(ret);
-                                    }
-                                }
-                            }
-                        }
-                        break;
-                }
+                let addedAnimes = await bookmarkFetches[site.siteName](site, 1, tryAddAnime);
 
                 if (addedAnimes.length > 0 && bookmarkAutoNotification) {
                     let notification = "Bookmarks: Added " + addedAnimes.length + "\n";
@@ -156,3 +60,72 @@ async function processBookmarkLoopBody(site, body) {
         });
     });
 }
+
+let bookmarkFetches = {
+    "kickassanime": async (site, page, tryAddAnime) => {
+
+        return fetch('https://www2.kickassanime.ro/api/get_anime_list/sub/' + page, {
+            method: 'GET'
+        })
+            .then(async response => response.json())
+            .then(async response => {
+                let addedAnimes = [];
+                for (let anime of response.data) {
+                    let ret = await tryAddAnime(site.prefixURL + anime.slug);
+                    if (ret) {
+                        addedAnimes.push(ret);
+                    }
+                }
+                return addedAnimes;
+            })
+            .catch(err => { console.error(err); return []; });
+    },
+
+    "9anime": async (site, page, tryAddAnime) => {
+
+        return fetch('https://9anime.id/ajax/home/widget/updated-sub?page=' + page, {
+            method: 'GET',
+            headers: { cookie: 'session=qMmVFNmjiK1GXm12A2gGboNnLoqH5fykSv6Nif6T' }
+        })
+            .then(async response => response.json())
+            .then(async response => {
+                let addedAnimes = [];
+                let regex = /<a\s+href="(.*?)">/g
+                let match = undefined;
+                while (match = regex.exec(response.result)) {
+                    let ret = await tryAddAnime(site.prefixURL + match[1]);
+                    if (ret) {
+                        addedAnimes.push(ret);
+                    }
+                }
+                return addedAnimes;
+            })
+            .catch(err => { console.error(err); return []; });
+    },
+
+    "gogoanimehub": async (site, page, tryAddAnime) => {
+
+        return fetch('https://gogoanimeapp.com//page-recent-release.html?type=1&page=' + page, {
+            method: 'GET',
+            headers: { cookie: 'gogoanime=9o5vj4kvomttllbh8s6s7ilb35' }
+        })
+            .then(async response => response.text())
+            .then(async response => {
+                let addedAnimes = [];
+                let regex = /<a\s+href="(\/.*?)">/g
+                let match = undefined;
+                let checkedUrls = [];
+                while (match = regex.exec(response)) {
+                    if (!checkedUrls.includes(match[1])) {
+                        checkedUrls.push(match[1]);
+                        let ret = await tryAddAnime(site.prefixURL + match[1]);
+                        if (ret) {
+                            addedAnimes.push(ret);
+                        }
+                    }
+                }
+                return addedAnimes;
+            })
+            .catch(err => { console.error(err); return []; });
+    }
+};
